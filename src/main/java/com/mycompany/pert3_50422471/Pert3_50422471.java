caf3b18375f9507a1f01e4ca46ce7c7195d4bb5e
Estermei <meichikinan@gmail.com>
2025-11-30 18:22:47 +0700

    first commit

---
+++ src/main/java/com/mycompany/pert3_50422471/Pert3_50422471.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pert3_50422471;
+
+/**
+ *
+ * @author meich
+ */
+public class Pert3_50422471 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
